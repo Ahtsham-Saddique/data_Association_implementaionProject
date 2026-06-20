d4c4bb698e52ec0b02e6752ec90d9290fab76e6d
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-06-20 10:42:39 +0500

    updated the user model

--- models/post.js
+++ models/post.js
@@ -1,28 +1,22 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/postdata');
 
-const postSchema = mongoose.Schema(
-    {
-        
-        user : {
-            type:monggose.Schema.Types.ObjectId,
-            ref:"user"
+const postSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    }]
+});
 
-        }
-        ,
-        date:
-        {
-            type:Date,
-            default:Date.now
-        },
-
-        content:String,
-        likes:
-        [{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"user"
-        }]
-    }
-)
-
-module.exports = mongoose.model('post',postSchema)
+module.exports = mongoose.model('post', postSchema);
